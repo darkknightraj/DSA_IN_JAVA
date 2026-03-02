9e4e46832e4ef10ade99a8c95aedb629515442ae
darkknightraj <rajankumarm30@gmail.com>
2026-03-02 08:01:33 +0530

    Added remove duplicate

--- TwoPointer/TwoSumBrute.java
+++ TwoPointer/TwoSumBrute.java
@@ -1,6 +1,5 @@
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
 
 public class TwoSumBrute {
     public static void main(String[] args) {
---
+++ TwoPointer/removeduplicate.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class removeduplicate{
+    public static void main(String[] args) {
+        int [] arr = {2,2,3,3,4,42,45};
+        int len = duplicate(arr);
+        System.out.println(len);
+        int [] res = Arrays.copyOf(arr, len);
+      //  int fianl_res = arr.length-res; to count the duplicate element 
+      // if only want unique than return slow + 1 ; 
+        System.out.println((Arrays.toString(res)));
+
+    }
+    public static int duplicate (int [] arr){
+        int slow = 0;
+        for(int fast = 1 ;fast < arr.length;fast++ ){
+            if(arr[slow] != arr[fast]){
+                slow++;
+                arr[slow]=arr[fast];
+
+            }
+          //  System.out.println(Arrays.toString(arr)); 
+        }
+        //return slow+1; to return the numbe of unique element for m the array in soting order 
+        return slow+1;      
+}
+}
